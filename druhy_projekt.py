85df9bddd8a272e7b0d3a64c38d64ff42c14926d
Lukasz Orszulik <luki93@seznam.cz>
2023-09-14 23:05:43 +0200

    edited conditions for entering the number

--- druhy_projekt.py
+++ druhy_projekt.py
@@ -16,9 +16,10 @@ sline = "-" * 50
 game_on = True
 
 #opakující se smyčka - hra
+print("Hi there!\n" + sline)
 while game_on:
     #TODO pozdrav
-    print("Hi there!\n" + sline + "\nI`ve generated a random 4 digit number for you. \nLet's play a bulls and cows game.")
+    print("I`ve generated a random 4 digit number for you. \nLet's play a bulls and cows game.")
     print(sline)
     a_number = random.randint(1000, 9999)
     print(a_number)
@@ -26,14 +27,18 @@ while game_on:
     while True:
         (guess_number := input("Enter a number: "))
         #podminky, hlidani aby byly cisla 4, nezacinala 0 a byly všechny numerické
-        if not guess_number.isnumeric() and len(guess_number) != 4 and guess_number[0] == "0":
+        if not guess_number.isnumeric():
             print("You must type only digits")
+        elif len(guess_number) != 4:
+            print("The number must be 4 digits long")
+        elif guess_number[0] == "0":
+            print("The first digit must be between 1 - 9")
         else:
             #TODO smyčka for bude iterovat hadane cislo a porovnavat s inputem uzivatele
+            print("OK")
         
         
-        
-        break
+            break
     break
 
 #TODO Hráč hádá číslo. Program jej upozorní, pokud zadá číslo kratší nebo delší než 4 čísla, 
