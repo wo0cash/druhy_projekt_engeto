f759bc48d0a7ef4f1c691ae8fed5dfb3c3430d26
Lukasz Orszulik <luki93@seznam.cz>
2023-09-30 12:42:45 +0200

    project renamed

---
+++ tic_tac.py
@@ -0,0 +1,11 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+--- Tic-tac-toe ---
+author: Lukasz Orszulik
+email: luki93@seznam.cz
+discord: Lukasz Orszulik, wo0cash
+"""
+import random
+import time
+import datetime
