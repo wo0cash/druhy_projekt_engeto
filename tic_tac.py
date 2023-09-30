5ca1790914d46ead4825146d79252e7740951285
Lukasz Orszulik <luki93@seznam.cz>
2023-09-30 12:48:54 +0200

    basics

--- tic_tac.py
+++ tic_tac.py
@@ -9,3 +9,17 @@ discord: Lukasz Orszulik, wo0cash
 import random
 import time
 import datetime
+print("""
+Welcome to Tic Tac Toe
+========================================
+GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a:
+* horizontal,
+* vertical or
+* diagonal row
+========================================""")
+
+print("Let\'s start the game\n" + "-" * 40)
