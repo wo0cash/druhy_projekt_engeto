029fc91c87325eedde7135e479755ba17bd666fc
Lukasz Orszulik <luki93@seznam.cz>
2023-10-09 17:45:56 +0200

    making code prettier

--- tic_tac.py
+++ tic_tac.py
@@ -18,7 +18,26 @@ def grid():
         |{board[6] :^3}|{board[7] :^3}|{board[8] :^3}| 
         +---+---+---+  
           """)
-
+def winner():
+    #condition checking whether one of player won
+        for i in range(0, 9, 3):
+            if board[i:i+3] == ["o", "o", "o"]:
+                print("Player o won!")
+            elif board[i:i+3] == ["x", "x", "x"]:
+                print("Player x won!")
+        for i in range(0, 3):
+            if board[i:i+7:3] == ["o", "o", "o"]:
+                print("Player o won! |")
+            elif board[i:i+7:3] == ["x", "x", "x"]:
+                print("Player x won! |")
+        if board[::4] == ["o", "o", "o"]:
+            print("Player o won! \\")
+        elif board[::4] == ["x", "x", "x"]:
+            print("Player x won! \\")
+        if board[2:7:2] == ["o", "o", "o"]:
+            print("Player o won! /")
+        if board[2:7:2] == ["x", "x", "x"]:
+            print("Player x won! /")
 
 print("""
 Welcome to Tic Tac Toe
@@ -39,65 +58,44 @@ while game_on:
     round = 1
     while round < 9:
         grid()
-        print(f"{round}. round")
+        print(f"{round}. round\n{'-' * 40}")
         round += 1
         
-        
-        #condition checking whether one of player won
-        for i in range(0, 9, 3):
-            if board[i:i+3] == ["o", "o", "o"]:
-                print("Player o won!")
-                print(grid)
-            elif board[i:i+3] == ["x", "x", "x"]:
-                print("Player x won!")
-                print(grid)
-        for i in range(0, 3):
-            if board[i:i+7:3] == ["o", "o", "o"]:
-                print("Player o won! |")
-                print(grid)
-            elif board[i:i+7:3] == ["x", "x", "x"]:
-                print("Player x won! |")
-                print(grid)
-        if board[::4] == ["o", "o", "o"]:
-            print("Player o won! \\")
-        elif board[::4] == ["x", "x", "x"]:
-            print("Player x won! \\")
-        
-        if board[2:7:2] == ["o", "o", "o"]:
-            print("Player o won! /")
-        if board[2:7:2] == ["x", "x", "x"]:
-            print("Player x won! /")
-        
-        #player o input
-        player_o = int(input("Player o | Your move number: "))
-        #range condition
-        if player_o == 0 or player_o > 9:
-            print("You can place your stone only between numbers 1 to 9!")
-        else:
-            player_o -= 1
-            if board[player_o] != "":
-                print("\n","!" * 30, "\nYou can`t put your stone here\n!!!")
+        pl_o = True
+        while pl_o:
+            #player o input
+            player_o = int(input("Player o | Your move number: "))
+            #range condition
+            if player_o == 0 or player_o > 9:
+               print("You can place your stone only between numbers 1 to 9!")
             else:
-                board[player_o] = "o"   # umístění kamenu
-        
-        print(grid())
-        
-        #player x input
-        player_x = int(input("Player x | Your move number: "))
-        #range condition
-        if player_x == 0 or player_x > 9:
-            print("You can place your mark only between numbers 1 to 9!")
+                player_o -= 1
+                if board[player_o] != "":
+                    print("\n","!" * 30, "\nYou can`t put your stone here\n!!!")
+                else:
+                    board[player_o] = "o"   # umístění kamenu
+                    pl_o = False
+        grid()
+        if winner():
+            winner()
+            round = False
         else:
-            player_x -= 1
-            if board[player_x] != "":
-                print("\n","!" * 30, "\nYou can`t put your mark here\n!!!")
-            else:
-                board[player_x] = "x"   # umístění kamenu
-                
-        
-        
-#TODO input uživatele 2
-
-
-
-#TODO výhra
+            pl_x = True
+            while pl_x:
+                #player x input
+                player_x = int(input("Player x | Your move number: "))
+                #range condition
+                if player_x == 0 or player_x > 9:
+                    print("You can place your mark only between numbers 1 to 9!")
+                else:
+                    player_x -= 1
+                    if board[player_x] != "":
+                        print("\n","!" * 30, "\nYou can`t put your mark here\n!!!")
+                    else:
+                        board[player_x] = "x"   # umístění kamenu
+                        if winner():
+                            grid()
+                            winner()
+                        else:
+                            pl_x = False
+    print("What the fuck")                
