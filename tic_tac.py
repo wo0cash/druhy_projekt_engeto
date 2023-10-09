f3c5e6c5a6481bd3482890f396ec7b6d146a908b
Lukasz Orszulik <luki93@seznam.cz>
2023-10-09 17:03:35 +0200

    adding other player, edited conditions for winning

--- tic_tac.py
+++ tic_tac.py
@@ -7,6 +7,19 @@ email: luki93@seznam.cz
 discord: Lukasz Orszulik, wo0cash
 """
 
+board = ["","","","","","","","",""]
+def grid():
+    return print(f"""
+        +---+---+---+
+        |{board[0] :^3}|{board[1] :^3}|{board[2] :^3}|
+        +---+---+---+
+        |{board[3] :^3}|{board[4] :^3}|{board[5] :^3}|   
+        +---+---+---+         
+        |{board[6] :^3}|{board[7] :^3}|{board[8] :^3}| 
+        +---+---+---+  
+          """)
+
+
 print("""
 Welcome to Tic Tac Toe
 ========================================
@@ -20,27 +33,15 @@ marks in a:
 * diagonal row
 ========================================""")
 
-
-
-#TODO hra smyčka dokud hra neskončí
-
-board = ["","","","","","","","",""]
-
 game_on = True
 while game_on:
     print("Let\'s start the game\n" + "-" * 40)
-    round = True
-    while round:
-        grid = f"""
-        +---+---+---+
-        |{board[0] :^3}|{board[1] :^3}|{board[2] :^3}|
-        +---+---+---+
-        |{board[3] :^3}|{board[4] :^3}|{board[5] :^3}|   
-        +---+---+---+         
-        |{board[6] :^3}|{board[7] :^3}|{board[8] :^3}| 
-        +---+---+---+  
-          """
-        print(grid)
+    round = 1
+    while round < 9:
+        grid()
+        print(f"{round}. round")
+        round += 1
+        
         
         #condition checking whether one of player won
         for i in range(0, 9, 3):
@@ -57,24 +58,42 @@ while game_on:
             elif board[i:i+7:3] == ["x", "x", "x"]:
                 print("Player x won! |")
                 print(grid)
-        for i in range(0, 3, 2):
-            if board[i:i+9: 4] == ["o", "o", "o"]:
-                print("Player o won! opacna")
-            elif board[i:7:2] == ["o", "o", "o"]:
-                print("Player o won! /")
-        #player`s input
-        player_o = int(input("Player o | Please enter your move number: "))
+        if board[::4] == ["o", "o", "o"]:
+            print("Player o won! \\")
+        elif board[::4] == ["x", "x", "x"]:
+            print("Player x won! \\")
+        
+        if board[2:7:2] == ["o", "o", "o"]:
+            print("Player o won! /")
+        if board[2:7:2] == ["x", "x", "x"]:
+            print("Player x won! /")
+        
+        #player o input
+        player_o = int(input("Player o | Your move number: "))
         #range condition
         if player_o == 0 or player_o > 9:
-            print("You can place your stone only between 1 to 9!")
+            print("You can place your stone only between numbers 1 to 9!")
         else:
             player_o -= 1
-            #TODO symčka ověřující, že jsou 3 kameny h/v/diag jinak požádá o umístění jinde
             if board[player_o] != "":
                 print("\n","!" * 30, "\nYou can`t put your stone here\n!!!")
             else:
-                board[player_o] = "o"
-                #TODO umístění kamenu
+                board[player_o] = "o"   # umístění kamenu
+        
+        print(grid())
+        
+        #player x input
+        player_x = int(input("Player x | Your move number: "))
+        #range condition
+        if player_x == 0 or player_x > 9:
+            print("You can place your mark only between numbers 1 to 9!")
+        else:
+            player_x -= 1
+            if board[player_x] != "":
+                print("\n","!" * 30, "\nYou can`t put your mark here\n!!!")
+            else:
+                board[player_x] = "x"   # umístění kamenu
+                
         
         
 #TODO input uživatele 2
