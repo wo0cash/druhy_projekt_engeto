32f784302a6bbe938a7c158ded4cea75bf1ace86
wo0cash <139061286+wo0cash@users.noreply.github.com>
2023-10-27 15:12:17 +0200

    game_round fuction

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -9,8 +9,9 @@ discord: Lukasz Orszulik, wo0cash
 import random
 import time
 import datetime
-#---Time formatting function
+
 def time_format(start, stop):
+    """Time formatting function"""
     el_time = int(round((stop - start), 0))
     el_time_min = el_time // 60
     el_time_sec = el_time % 60
@@ -23,8 +24,12 @@ def num_gen():
     print(a) #just for checking
     return a, a_list
 
-def bull_cow():
+def bull_cow(a_number, b_number):
     """Conditions whether it\'s bull or cow"""
+    a_list = list(enumerate(a_number)) #generating indexes for each number
+    b_list = list(enumerate(b_number))
+    cows = []
+    bulls = []
     for index, cislo in b_list:
         if cislo ==  a_list[index][1]:
             bulls.append(cislo)
@@ -33,23 +38,48 @@ def bull_cow():
                 cows.append(cislo)
     print(f"{len(bulls)} bulls, {len(cows)} cows")
     print(sline) 
+    return bulls
 
-def final_result():
-    """Final result"""    
-    print(f"Correct, you've guessed the right number in {guesses}. guess!") if guesses == 1 else print(f"Correct, you've guessed the right number in {guesses} guesses!")
-    if guesses < 4:
-        print("That`s amazing")
-    elif guesses < 7:
-        print("Average score")
+
+
+def final_result(bulls: int) -> str:
+    """Shows the final result - number of guesses"""    
+    if len(bulls) == 4:
+        print(f"Correct, you've guessed the right number in {guesses} guess!") if guesses == 1 else print(f"Correct, you've guessed the right number in {guesses} guesses!")
+        if guesses < 4:
+            print("That`s amazing")
+        elif guesses < 7:
+            print("Average score")
+        else:
+            print("Not so good...")
+        return False
     else:
-        print("Not so good...")
+        return True
     
-
-def guess_count():
-    """Counting number of guesses"""
-    global guesses
-    guesses += 1
-
+def game_round(input):
+    """Game round.
+    -> Counts numer of guesses
+    -> Checks the input (only digits, 4 digits,  from 1000 - 9999)
+    """
+    guesses = 0
+    #---Input form user -> guessing the number
+    game_round = True
+    while game_round:
+        #---Conditions, 4 digits, all digits, and [0] index != 0
+        if not input.isnumeric():
+            print("You must type only digits")
+        elif len(input) != 4:
+            print("The number must be 4 digits long")
+        elif input[0] == "0":
+            print("The number must be between 1000-9999")
+        else:
+            #---Counting guesses
+            guesses = guesses + 1
+            #---Conditions whether it's bull or cow
+            bulls = bull_cow(a_number, input)
+            #---Showing the final result
+            game_round = final_result(bulls)
+                
 def continue_game() -> bool:
     cont_game = True
     while cont_game: 
@@ -62,8 +92,11 @@ def continue_game() -> bool:
             return False
         else:
             print("Must type - 'y'/'n'")
+
 def score():
     """Writing score to score.txt file"""
+    date = datetime.datetime.now()
+    fdate = date.strftime("%x")
     txt_file = open("score.txt", mode="a")
     txt_file.write(f"\nDate: {fdate} | {user: <10}| Guesses: {guesses: <3}| {time_format(start_time, end_time)}")
     txt_file.close()
@@ -81,50 +114,18 @@ while game_on:
     if len(user) <= 0:
         print("You're name must be at least 1 character long!")
     else:
-        guesses = 0
         start_time = time.time()
         print(f"{sline}\nI`ve generated a random 4 digit number for you. \nLet's play a bulls and cows game.\n{sline}")
         a_number, a_list = num_gen()
         
-        #---Input form user -> guessing the number
-        while True:
-            b_number = input("Enter a number: ")
-            #---Conditions, 4 digits, all digits, and [0] index != 0
-            if not b_number.isnumeric():
-                print("You must type only digits")
-            elif len(b_number) != 4:
-                print("The number must be 4 digits long")
-            elif b_number[0] == "0":
-                print("The first digit must be between 1 - 9")
-            else:
-                #---Counting guesses
-                guesses = guesses + 1
-                a_list = list(enumerate(a_number)) #generating indexes for each number
-                b_list = list(enumerate(b_number))
-
-                cows = []
-                bulls = []
-                #---Conditions whether it's bull or cow
-                bull_cow()
-                      
-                
-                #---Showing the final result
-                if len(bulls) == 4:
-                    print(f"Correct, you've guessed the right number in {guesses} guess!") if guesses == 1 else print(f"Correct, you've guessed the right number in {guesses} guesses!")
-                    if guesses < 4:
-                        print("That`s amazing")
-                    elif guesses < 7:
-                        print("Average score")
-                    else:
-                        print("Not so good...")
-                    break
-                else:
-                    continue
-        #---Ending time counting
+        guesses = 0
+        
+        game_round(input("Enter a number: "))
+        
         end_time = time.time()
+        
         print(time_format(start_time, end_time))
-        date = datetime.datetime.now()
-        fdate = date.strftime("%x")
+        
         score()
 
     game_on = continue_game()
